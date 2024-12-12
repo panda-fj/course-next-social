88eee2c647a9c4272c86301617ec0e859a2e66e7
fj <ku99.en+p@gmail.com>
2024-12-12 23:44:58 +0900

    ~ 33:30  Mobile First Design with Tailwind CSS

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,14 +1,39 @@
+"use client"
+
+import Link from "next/link";
+import { useState } from "react";
+
 const MobileMenu = () => {
-  return (
-    <div className="">
-        {/* ハンバーガーメニュー */}
-        <div className="flex flex-col gap-[4.5px] cursor-pointer">
-            <div className="w-6 h-1 bg-blue-500 rounded-sm"/>
-            <div className="w-6 h-1 bg-blue-500 rounded-sm"/>
-            <div className="w-6 h-1 bg-blue-500 rounded-sm"/>
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <div className="md:hidden">
+            {/* ハンバーガーメニュー */}
+            <div
+                className="flex flex-col gap-[4.5px] cursor-pointer"
+                onClick={() => setIsOpen((prev) => !prev)}
+            >
+                <div className={`w-6 h-1 bg-blue-500 rounded-sm ${
+                    isOpen ? "rotate-45" : ""
+                } origin-left ease-in-out duration-500`} />
+                <div className={`w-6 h-1 bg-blue-500 rounded-sm ${
+                    isOpen ? "opacity-0" : ""
+                } ease-in-out duration-500`} />
+                <div className={`w-6 h-1 bg-blue-500 rounded-sm ${
+                    isOpen ? "-rotate-45" : ""
+                } origin-left ease-in-out duration-500`} />
+            </div>
+
+            {isOpen && (
+                <div className="absolute left-0 top-24 w-full h-[calc(100vh-96px)] bg-white flex flex-col items-center justify-center gap-8 font-medium text-lg z-10">
+                    <Link href="/">Home</Link>
+                    <Link href="/">Friends</Link>
+                    <Link href="/">Groups</Link>
+                    <Link href="/">Stories</Link>
+                    <Link href="/">Login</Link>
+                </div>
+            )}
         </div>
-    </div>
-  )
+    )
 }
 
 export default MobileMenu
