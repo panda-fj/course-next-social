d6d9f2c41b0898cb277cccc830f892d2163cf9d0
fj <ku99.en+p@gmail.com>
2024-12-13 16:24:37 +0900

    ~ 53:20 Next.js Social Media App Homepage Design

---
+++ src/components/Stories.tsx
@@ -0,0 +1,13 @@
+const Stories = () => {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-sm">
+        <div className="flex gap-8">
+            <div className="">
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Stories
